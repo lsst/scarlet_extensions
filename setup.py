99211b1a8a3ffa5aab0b6e4bd3ac850921b1f2ed
fred3m <fredem@princeton.edu>
2020-08-19 20:05:18 -0400

    fix version

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
 from setuptools import setup, find_packages
-import subprocess
-
-# Use the firt 7 digits of the git hash to set the version
-version_root = "0.0"
-try:
-    __version__ = version_root + subprocess.check_output(["git", "rev-parse", "HEAD"])[
-        :7
-    ].decode("utf-8")
-except:
-    __version__ = version_root
 
 setup(
     name="scarlet_extensions-fred3m",
